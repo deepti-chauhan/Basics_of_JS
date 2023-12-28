9e89a95f721638ca00f8b7f258325c1786676923
Deepti <deepti22599@gmail.com>
2023-12-28 18:03:24 +0530

    add generator example

--- generator.js
+++ generator.js
@@ -1,3 +1,11 @@
+
+/*
+Generators - special functions that allows you to pause its execution and then resume it whenever required.
+yield keyword pause the execution & next() funtion resume the call.
+
+returns iterator object : {value : someValue, done : true/false}
+*/
+
 function* fibonacci(limit) {
   let a = 0,
     b = 1
@@ -8,6 +16,7 @@ function* fibonacci(limit) {
 }
 
 const fibonacciIterator = fibonacci(5)
+
 for (const fib of fibonacciIterator) {
   console.log(fib)
 }
