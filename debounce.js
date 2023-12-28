4ececc6291b6fb3d105ba5a2ddbd9d972feecd66
Deepti <deepti22599@gmail.com>
2023-12-28 20:08:48 +0530

    add debouncing example

--- debounce.js
+++ debounce.js
@@ -1,1 +1,29 @@
-// WRITE
+
+
+/*
+  debouncing - 
+*/
+
+
+function searchHandler(query){
+    console.log(`your search query : ${query}`)
+  }
+  
+  
+  function deboundeFunc(func, delay) {
+    let timer
+    return function(...args) {
+    console.log('timer calling...');
+      clearTimeout(timer)
+      timer = setTimeout(()=>{
+        func.apply(this, args)
+      },delay)
+    }
+  }
+  
+  const deboundeHandler = deboundeFunc(searchHandler, 1000)
+  deboundeHandler('a')
+  deboundeHandler('ap')
+  deboundeHandler('app')
+  deboundeHandler('appl')
+  deboundeHandler('apple')
